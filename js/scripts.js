a1b92743a08aee09b72f9705887d5f4957c6344f
BrittneyM34 <brittneym1018@yahoo.com>
2023-09-05 11:37:19 -0400

    deleted alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
 let favoriteFood = 'pasta';
 document.write(favoriteFood);
+let name = 'John';
+let age = 32;
+let message = `Hello!
+This is a longer message here.
+My name is: ${name},
+and I'm ${age}.`; 
