dd629d348870cd46ea31e6e3ba1c531f0e0e6dfd
BrittneyM34 <brittneym1018@yahoo.com>
2023-09-19 13:38:11 -0400

    created pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
-let pokemonList = [
-{
-    name: 'Bulbasaur',
-    height: 7, 
-    weight: 6.9,
-    type: ['grass' , 'poison']
-},
-{
-    name: 'Charmander', 
-    height: '6', 
-    weight: 8.5,
-    type: ['fire']
-},
-{
-    name: 'Beedrill', 
-    height: 10,
-    weight: 29.5,
-    type: ['bug', 'poison']
-}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7, 
+        weight: 6.9,
+        type: ['grass' , 'poison']
+    },
+    {
+        name: 'Charmander', 
+        height: '6', 
+        weight: 8.5,
+        type: ['fire']
+    },
+    {
+        name: 'Beedrill', 
+        height: 10,
+        weight: 29.5,
+        type: ['bug', 'poison']
+    }
+    ];
 
-pokemonList.forEach (function(pokemon) {
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+pokemonRepository.getAll().forEach (function(pokemon) {
     document.write('<p>' + pokemon.name + ' (Height: ' + pokemon.height + ')');
     if (pokemon.height > 9) {
         document.write (' - Wow, that\'s big!');
